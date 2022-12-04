a1d80b5cdda7483fe0a640dbbc8212de95e56a1a
srivardhan17 <115682273+srivardhan17@users.noreply.github.com>
2022-12-04 15:29:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -297,5 +297,12 @@
          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
          -->
 </body>
+<script type='text/javascript' src='config.js'></script>
+<script>
 
+var token = config.MY_API_TOKEN;
+var key = config.SECRET_API_KEY;
+document.write(token);
+    console.log(key);
+</script>
 </html>
